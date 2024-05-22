f4e2c2906c3352e8ca0269ba63356168ccfaa0b4
Maarten <maarten.wils1234@gmail.com>
2024-05-22 10:10:22 +0200

    Changed welcome route

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,8 @@ use App\Http\Controllers\TokenController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('welcome');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
