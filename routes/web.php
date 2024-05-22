67355893e36e26c6046b2f5d1a3a42776dbb9b9f
Maarten <maarten.wils1234@gmail.com>
2024-05-22 10:10:22 +0200

    Changed welcome route

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,8 @@ use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return view('welcome');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
